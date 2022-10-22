9d0b560569767eb3dbf508332db78a411fb75c9f
fodela <particleme30@gmail.com>
2022-10-22 21:20:18 +0000

    test: add page

---
+++ Selenium/testing/page.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+
+from Selenium.testing.locators import MainPageLocators
+# Base page
+
+
+class BasePage():
+    def __init__(self, driver: webdriver) -> None:
+        self.driver = driver
+
+# Main page
+
+
+class MainPage(BasePage):
+    # check title
+    def is_keyword_in_title(self, keyword: str) -> bool:
+        return keyword in self.driver.title
+
+    def click_go_button(self) -> None:
+        go_button = self.driver.find_element(*MainPageLocators.GO_BUTTON)
+        go_button.click()
+
+
+class SearchResultPage(BasePage):
+    def is_results_found(self):
+        return "No results found" not in self.page_source
