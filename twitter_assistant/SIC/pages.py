2214099dd1df8c0a667f41bdffc985491937bea9
fodela <particleme30@gmail.com>
2022-10-24 22:13:13 +0000

    feat: update pages

--- twitter_assistant/SIC/pages.py
+++ twitter_assistant/SIC/pages.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
+import locators
+
 
 class BasePage():
     def __init__(self) -> None:
@@ -10,7 +12,14 @@ class TwitterHomePage(BasePage):
     def type_tweet(self, tweet: str):
         pass
 
-    def click_tweet_button(self) -> None:
-        tweet_button = self.driver.find_element(
-            *TwitterHomePageLocators.TWEET_BUTTON)
-        tweet_button.click()
+    def click_tweet_button(self, type: str) -> None:
+        locator = locators.TwitterHomePageLocator(type, "elemento")
+        # tweet_button = self.driver.find_element(
+        # *locator.element)
+        # tweet_button.click()
+        print(locator.element)
+
+
+if __name__ == "__main__":
+    program = TwitterHomePage()
+    program.click_tweet_button("name")
