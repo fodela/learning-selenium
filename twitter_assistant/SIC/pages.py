f4c27ab205671d24b28fa10f59453b4fba1eb0a5
fodela <particleme30@gmail.com>
2022-10-25 22:14:32 +0000

    refactor: pages

--- twitter_assistant/SIC/pages.py
+++ twitter_assistant/SIC/pages.py
@@ -14,10 +14,6 @@ class TwitterHomePage(BasePage):
 
     def click_tweet_button(self, type: str) -> None:
         locator = locators.TwitterHomePageLocator(type, "elemento")
-        # tweet_button = self.driver.find_element(
-        # *locator.element)
-        # tweet_button.click()
-        print(locator.element)
 
 
 if __name__ == "__main__":
