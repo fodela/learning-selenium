a4cfed5ff72388db09249a9155017cbeb466f857
fodela <particleme30@gmail.com>
2022-10-26 20:20:51 +0000

    feat: post a tweet -incomplete

--- twitter_assistant/SIC/main.py
+++ twitter_assistant/SIC/main.py
@@ -1,8 +1,21 @@
-def post_a_tweet(tweet: str) -> None:
-    # input tweet
+import os
+from selenium import webdriver
 
-    # click tweet button
-    pass
+from pages import TwitterHomePage
+
+PATH = "/usr/local/bin/chromedriver"
+os.chmod(PATH, 755)
+
+driver = webdriver.Chrome(PATH)
+
+
+def post_a_tweet():
+    try:
+        twitter = driver.get("https://twitter.com/home")
+        twitter_home_page = TwitterHomePage(driver)
+    finally:
+        # driver.close()
+        pass
 
 
 if __name__ == "__main__":
