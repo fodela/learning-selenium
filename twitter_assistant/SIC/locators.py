ebb04b406b30c18a5baffd93500bda83ac734f94
fodela <particleme30@gmail.com>
2022-10-26 20:21:26 +0000

    feat: add tweet button class

--- twitter_assistant/SIC/locators.py
+++ twitter_assistant/SIC/locators.py
@@ -2,5 +2,4 @@ from selenium.webdriver.common.by import By
 
 
 class TwitterHomePageLocator():
-    def __init__(self) -> None:
-        TWEET_BUTTON = (By.ID, "")
+    TWEET_BUTTON = (By.ID, "r-qvutc0")
